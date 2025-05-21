f9164a99c662b46042f7f01b760d85f5cf92ff75
mental2008 <yangly1999@gmail.com>
2025-05-21 19:56:32 +0800

    Update lora loading analysis

--- trace/adapter_loading_analysis.py
+++ trace/adapter_loading_analysis.py
@@ -25,8 +25,6 @@ def adapter_loading_analysis(trace_data, scene_id, cache_policy):
 
     ADAPTER_LOADING_FIG_PATH = os.path.join(BASE_FIG_DIR, f"adapter_loading_{cache_policy}_{scene_id}.pdf")
     print(f"ADAPTER_LOADING_FIG_PATH: {ADAPTER_LOADING_FIG_PATH}")
-    UNIQUE_ADDON_FIG_PATH = os.path.join(BASE_FIG_DIR, f"unique_loras_{scene_id}.pdf")
-    print(f"UNIQUE_ADDON_FIG_PATH: {UNIQUE_ADDON_FIG_PATH}")
 
     # Get the unique IPs
     ip_list = []
@@ -219,42 +217,7 @@ def adapter_loading_analysis(trace_data, scene_id, cache_policy):
         raise ValueError("Invalid scene_id")
     plt.grid(alpha=.3, linestyle='--')
     plt.savefig(ADAPTER_LOADING_FIG_PATH, bbox_inches='tight')
-
-
-    # Analyze the unique addon numbers
-    print("=====Unique LoRAs=====")
-    df = pd.DataFrame()
-    for ip in ip_req_dict:
-        unique_controlnets = set()
-        for req in ip_req_dict[ip]:
-            controlnets = req["controlnets"]
-            if controlnets is None or len(controlnets) == 0:
-                continue
-            unique_controlnets.update([controlnet for controlnet in controlnets])
-
-        unique_loras = set()
-        for req in ip_req_dict[ip]:
-            loras = req["loras"]
-            if loras is None or len(loras) == 0:
-                continue
-            unique_loras.update([lora for lora in loras])
-
-        df = pd.concat([df, pd.DataFrame({
-            "ip": [ip], 
-            "num_requests": [len(ip_req_dict[ip])], 
-            "unique_controlnets": [len(unique_controlnets)], 
-            "unique_loras": [len(unique_loras)]
-        })], ignore_index=True)
-
-    # Plot the unique addon numbers
-    plt.figure(figsize=(7, 3), dpi=120)
-    # plt.scatter(df["num_requests"], df["unique_controlnets"], marker='o', label="ControlNet")
-    plt.scatter(df["num_requests"], df["unique_loras"], marker='^', label="LoRA")
-    plt.xlabel("# of requests")
-    # plt.ylabel("# of unique\naddons", loc='top')
-    plt.ylabel("# of unique\nLoRAs", loc='top')
-    plt.grid(alpha=.3, linestyle='--')
-    plt.savefig(UNIQUE_ADDON_FIG_PATH, bbox_inches='tight')
+    print(f"Figure saved to {ADAPTER_LOADING_FIG_PATH}")
 
 
 if __name__ == "__main__":
